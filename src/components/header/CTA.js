6f078563013e094f15d65f166255bea4834c06d8
Collins Abu <64439462+collinsabu@users.noreply.github.com>
2024-08-31 13:03:25 +0100

    updated link

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -4,7 +4,7 @@ import "./header.css"
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://drive.google.com/file/d/1Wl0udGzxO1uwqXNN8WMOWgg84eduWgbd/view?usp=sharing" target='_blank' className='btn new'>Download CV</a>
+      <a href="https://drive.google.com/file/d/1Wl0udGzxO1uwqXNN8WMOWgg84eduWgbd/view?usp=sharing" target='_blank' className='btn '>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )

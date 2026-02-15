a3dcef12beb7171a8baf17861529c941c3765311
Collins Abu <64439462+collinsabu@users.noreply.github.com>
2026-02-15 12:35:25 +0000

    i updated my CV download link

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -4,7 +4,7 @@ import "./header.css"
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://drive.google.com/file/d/16f0MQlXtBwSlRXl-hyOctkUMKxt84WEz/view?usp=sharing" target='_blank' className='btn '>Download CV</a>
+      <a href="https://drive.google.com/file/d/1jhAI2P-rOkqiBffWGxiTc5mUAJ6yBG2g/view?usp=sharing" target='_blank' className='btn '>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
